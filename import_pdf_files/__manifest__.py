5884371a96934c313e3e546970632a824dcf2367
Todoo S.A.S <46287521+infotodoo@users.noreply.github.com>
2020-01-30 17:23:59 -0500

    import model

---
+++ import_pdf_files/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Import PDF file From URL',
+    'description': """
+New extensible PDF file import from URL in CSV for Odoo
+======================================
+
+Import PDF file in binary fields from URL into any table
+""",
+    'version': '10.0.1.0.0',
+    'author': "Techspawn Solutions",
+    'website': "https://techspawn.com",
+    'depends': ['base_import'],
+    'category': 'Extra Tools',
+    'installable': True,
+    'auto_install': True,
+    'data': [],
+    'images':['static/description/background.jpg'],
+}
