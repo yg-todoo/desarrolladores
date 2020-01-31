18e9b67ebf04b8b5bc7323446735b8705c250cc6
Todoo S.A.S <46287521+infotodoo@users.noreply.github.com>
2020-01-31 09:00:30 -0500

    from.

--- import_pdf_files/models/base_import.py
+++ import_pdf_files/models/base_import.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import logging
-import urllib2
-import base64
+from. import logging
+from. import urllib2
+from. import base64
 from odoo import api, fields, models,_
 
 
